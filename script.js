bd197d17715af5ca3aeb4604b572d2727f6ba313
Mohammed Sahal <technicalsahal25@gmail.com>
2023-06-19 12:09:31 +0530

    Completed

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const bar = document.getElementById("menu");
 bar.addEventListener("click",()=>{
     nav.classList.toggle("jav")
 });
+
+var icon = document.getElementById('mobilemenu')
+var hiddenBox = document.getElementById('hidden-div')
+
+icon.addEventListener('click',()=>{
+    hiddenBox.classList.toggle('hidden')
+})
+var closeIcon = document.getElementById('mmm')
+closeIcon.addEventListener('click',()=>{
+    if(hiddenBox.classList.contains('hidden')){
+        hiddenBox.classList.remove('hidden')
+    }
+})
